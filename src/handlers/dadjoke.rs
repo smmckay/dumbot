b67616a787c2f70d0156b75c3c978b0f03f0314b
Steve McKay <steve@b.abbies.us>
2019-03-08 10:46:56 -0500

    setup...punchline

--- src/handlers/dadjoke.rs
+++ src/handlers/dadjoke.rs
@@ -3,21 +3,52 @@ use rand::Rng;
 use regex::Regex;
 use serenity::client::Context;
 use serenity::model::channel::Message;
+use std::thread;
+use std::time::Duration;
 use threadpool::ThreadPool;
+use serenity::model::id::ChannelId;
+use serenity::prelude::SerenityError;
 
 pub struct Handler {
     re: Regex,
-    jokes: Vec<String>
+    jokes: Vec<Joke>
+}
+
+#[derive(Clone)]
+struct Joke {
+    setup: String,
+    punchline: String
+}
+
+impl Joke {
+    fn new(setup: &str, punchline: &str) -> Joke {
+        Joke {
+            setup: setup.to_owned(),
+            punchline: punchline.to_owned()
+        }
+    }
 }
 
 impl MessageHandler for Handler {
-    fn message(&self, _: &Context, msg: &Message, _: &ThreadPool) -> bool {
+    fn message(&self, _: &Context, msg: &Message, pool: &ThreadPool) -> bool {
         if self.re.is_match(msg.content.as_str()) {
+            let channel = msg.channel_id;
             let idx = rand::thread_rng().gen_range(0, self.jokes.len());
-            let text = self.jokes.get(idx).unwrap();
-            if let Err(why) = msg.channel_id.send_message(|m| m.content(text)) {
-                warn!("Msg send failed: {:?}", why)
-            }
+            let joke = self.jokes.get(idx).unwrap().clone();
+
+            pool.execute(move || {
+                if let Err(why) = (|| -> Result<(), SerenityError> {
+                    let delay: u64 = rand::thread_rng().gen_range(500, 1500);
+                    send_msg_after_delay(channel, delay, &joke.setup)?;
+                    if joke.punchline.len() > 0 {
+                        send_msg_after_delay(channel, delay, &joke.punchline)?;
+                    }
+                    Ok(())
+                })() {
+                    warn!("API call failed: {:?}", why);
+                }
+            });
+
             true
         } else {
             false
@@ -28,49 +59,56 @@ impl MessageHandler for Handler {
 impl Handler {
     pub fn new() -> Handler {
         Handler {
-            re: Regex::new("dad\\s*jo(?:ke|ek)").unwrap(),
+            re: Regex::new("(?i:dad\\s*jo(?:ke|ek))").unwrap(),
             jokes: vec![
-                "Did you hear about the restaurant on the moon? Great food, no atmosphere.".to_owned(),
-                "What do you call a fake noodle? An Impasta.".to_owned(),
-                "How many apples grow on a tree? All of them.".to_owned(),
-                "Want to hear a joke about paper? Nevermind it's tearable.".to_owned(),
-                "I just watched a program about beavers. It was the best dam program I've ever seen.".to_owned(),
-                "Why did the coffee file a police report? It got mugged.".to_owned(),
-                "How does a penguin build it's house? Igloos it together.".to_owned(),
-                "Dad, did you get a haircut? No I got them all cut.".to_owned(),
-                "What do you call a Mexican who has lost his car? Carlos.".to_owned(),
-                "Dad, can you put my shoes on? No, I don't think they'll fit me.".to_owned(),
-                "Why did the scarecrow win an award? Because he was outstanding in his field.".to_owned(),
-                "Why don't skeletons ever go trick or treating? Because they have no body to go with.".to_owned(),
-                "Ill call you later. Don't call me later, call me Dad.".to_owned(),
-                "What do you call an elephant that doesn't matter? An irrelephant".to_owned(),
-                "Want to hear a joke about construction? I'm still working on it.".to_owned(),
-                "What do you call cheese that isn't yours? Nacho Cheese.".to_owned(),
-                "Why couldn't the bicycle stand up by itself? It was two tired.".to_owned(),
-                "What did the grape do when he got stepped on? He let out a little wine.".to_owned(),
-                "I wouldn't buy anything with velcro. It's a total rip-off.".to_owned(),
-                "The shovel was a ground-breaking invention.".to_owned(),
-                "Dad, can you put the cat out? I didn't know it was on fire.".to_owned(),
-                "This graveyard looks overcrowded. People must be dying to get in there.".to_owned(),
-                "Whenever the cashier at the grocery store asks my dad if he would like the milk in a bag he replies, \"No, just leave it in the carton!".to_owned(),
-                "5/4 of people admit that they’re bad with fractions.".to_owned(),
-                "Two goldfish are in a tank. One says to the other, \"do you know how to drive this thing?".to_owned(),
-                "What do you call a man with a rubber toe? Roberto.".to_owned(),
-                "What do you call a fat psychic? A four-chin teller.".to_owned(),
-                "I would avoid the sushi if I was you. It’s a little fishy.".to_owned(),
-                "To the man in the wheelchair that stole my camouflage jacket... You can hide but you can't run.".to_owned(),
-                "The rotation of earth really makes my day.".to_owned(),
-                "I thought about going on an all-almond diet. But that's just nuts".to_owned(),
-                "What's brown and sticky? A stick.".to_owned(),
-                "I’ve never gone to a gun range before. I decided to give it a shot!".to_owned(),
-                "Why do you never see elephants hiding in trees? Because they're so good at it.".to_owned(),
-                "Did you hear about the kidnapping at school? It's fine, he woke up.".to_owned(),
-                "A furniture store keeps calling me. All I wanted was one night stand.".to_owned(),
-                "I used to work in a shoe recycling shop. It was sole destroying.".to_owned(),
-                "Did I tell you the time I fell in love during a backflip? I was heels over head.".to_owned(),
-                "I don’t play soccer because I enjoy the sport. I’m just doing it for kicks.".to_owned(),
-                "People don’t like having to bend over to get their drinks. We really need to raise the bar.".to_owned()
+                Joke::new("Did you hear about the restaurant on the moon?", "Great food, no atmosphere."),
+                Joke::new("What do you call a fake noodle?", "An Impasta."),
+                Joke::new("How many apples grow on a tree?", "All of them."),
+                Joke::new("Want to hear a joke about paper?", "Nevermind it's tearable."),
+                Joke::new("I just watched a program about beavers.", "It was the best dam program I've ever seen."),
+                Joke::new("Why did the coffee file a police report?", "It got mugged."),
+                Joke::new("How does a penguin build it's house?", "Igloos it together."),
+                Joke::new("Dad, did you get a haircut?", "No I got them all cut."),
+                Joke::new("What do you call a Mexican who has lost his car?", "Carlos."),
+                Joke::new("Dad, can you put my shoes on?", "No, I don't think they'll fit me."),
+                Joke::new("Why did the scarecrow win an award?", "Because he was outstanding in his field."),
+                Joke::new("Why don't skeletons ever go trick or treating?", "Because they have no body to go with."),
+                Joke::new("Ill call you later.", "Don't call me later, call me Dad."),
+                Joke::new("What do you call an elephant that doesn't matter?", "An irrelephant"),
+                Joke::new("Want to hear a joke about construction?", "I'm still working on it."),
+                Joke::new("What do you call cheese that isn't yours?", "Nacho Cheese."),
+                Joke::new("Why couldn't the bicycle stand up by itself?", "It was two tired."),
+                Joke::new("What did the grape do when he got stepped on?", "He let out a little wine."),
+                Joke::new("I wouldn't buy anything with velcro.", "It's a total rip-off."),
+                Joke::new("The shovel was a ground-breaking invention.", ""),
+                Joke::new("Dad, can you put the cat out?", "I didn't know it was on fire."),
+                Joke::new("This graveyard looks overcrowded.", "People must be dying to get in there."),
+                Joke::new("Whenever the cashier at the grocery store asks my dad if he would like the milk in a bag he replies, \"No, just leave it in the carton!\"", ""),
+                Joke::new("5/4 of people admit that they’re bad with fractions.", ""),
+                Joke::new("Two goldfish are in a tank. One says to the other, \"Do you know how to drive this thing?\"", ""),
+                Joke::new("What do you call a man with a rubber toe?", "Roberto."),
+                Joke::new("What do you call a fat psychic?", "A four-chin teller."),
+                Joke::new("I would avoid the sushi if I was you.", "It’s a little fishy."),
+                Joke::new("To the man in the wheelchair that stole my camouflage jacket...", "You can hide but you can't run."),
+                Joke::new("The rotation of earth really makes my day.", ""),
+                Joke::new("I thought about going on an all-almond diet.", "But that's just nuts"),
+                Joke::new("What's brown and sticky?", "A stick."),
+                Joke::new("I’ve never gone to a gun range before.", "I decided to give it a shot!"),
+                Joke::new("Why do you never see elephants hiding in trees?", "Because they're so good at it."),
+                Joke::new("Did you hear about the kidnapping at school?", "It's fine, he woke up."),
+                Joke::new("A furniture store keeps calling me.", "All I wanted was one night stand."),
+                Joke::new("I used to work in a shoe recycling shop.", "It was sole destroying."),
+                Joke::new("Did I tell you the time I fell in love during a backflip?", "I was heels over head."),
+                Joke::new("I don’t play soccer because I enjoy the sport.", "I’m just doing it for kicks."),
+                Joke::new("People don’t like having to bend over to get their drinks.", "We really need to raise the bar.")
             ]
         }
     }
+}
+
+fn send_msg_after_delay(channel: ChannelId, millis: u64, text: &String) -> Result<(), SerenityError> {
+    channel.broadcast_typing()?;
+    thread::sleep(Duration::from_millis(millis));
+    channel.send_message(|m| m.content(text))?;
+    Ok(())
 }
